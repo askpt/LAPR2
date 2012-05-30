0f971b87972180f8115176f08dc8ef97d31363ac
vitorhugoanjos <vitorhugoanjosowns@gmail.com>
2012-05-30 21:53:58 +0000

    Update
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@23 d26f016a-5ee3-6924-6cc9-f88242385996

--- src/gui/Teste.java
+++ src/gui/Teste.java
@@ -1,7 +1,15 @@
 package gui;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class Teste extends JFrame{
 	
@@ -21,10 +29,21 @@ public class Teste extends JFrame{
         menuBar.add( menu );
         
         menuItem = new JMenuItem( "Import" );
+        menuItem.addActionListener( new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+            }
+     } );
         menu.add(menuItem);
         
         menuItem = new JMenuItem( "Export" );
+        menuItem.addActionListener( new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+            }
+     } );
         menu.add(menuItem);
+        
         setJMenuBar( menuBar );
         
         setDefaultCloseOperation( EXIT_ON_CLOSE );
