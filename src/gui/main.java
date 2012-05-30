2a7aea933216067f76444647ed600113df06371f
askpt <andres.silva010@gmail.com>
2012-05-30 12:01:55 +0000

    Criação da classe principal e da classe de ficheiros csv. Criação da package de interface gráfica e dos ficheiros.
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@19 d26f016a-5ee3-6924-6cc9-f88242385996

---
+++ src/gui/main.java
@@ -0,0 +1,15 @@
+package gui;
+
+import jogosolimpicos.*;
+import listaligada.*;
+
+public class main {
+
+	private static ListaLigada<Atleta> atleta = new ListaLigada<Atleta>();
+	private static ListaLigada<Pais> paises = new ListaLigada<Pais>();
+
+	public static void main(String[] args) {
+
+	}
+
+}
