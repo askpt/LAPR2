3a19037cae51f135197cb43c74d44a68081dc5d9
askpt <andres.silva010@gmail.com>
2012-05-30 11:45:28 +0000

    Documentação das classes Medalhas e Pais.
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@18 d26f016a-5ee3-6924-6cc9-f88242385996

--- src/jogosolimpicos/Medalhas.java
+++ src/jogosolimpicos/Medalhas.java
@@ -1,42 +1,87 @@
 package jogosolimpicos;
 
+/**
+ * Class of the management of the medals.
+ */
+
 public class Medalhas {
 
 	private int ouro, prata, bronze;
 
+	/**
+	 * Main constructor of class medals. Initializes the gold, silver and bronze
+	 * medals to zero.
+	 */
 	public Medalhas() {
 		this.ouro = 0;
 		this.prata = 0;
 		this.bronze = 0;
 	}
 
+	/**
+	 * Get the gold medals.
+	 * 
+	 * @return gold medals
+	 */
+
 	public int getOuro() {
 		return ouro;
 	}
+
+	/**
+	 * Get the silver medals.
+	 * 
+	 * @return silver medals
+	 */
 
 	public int getPrata() {
 		return prata;
 	}
 
+	/**
+	 * Get the bronze medals.
+	 * 
+	 * @return bronze medals
+	 */
+
 	public int getBronze() {
 		return bronze;
 	}
+
+	/**
+	 * Add one medal to the gold medals.
+	 */
 
 	public void addOuro() {
 		this.ouro += 1;
 	}
 
+	/**
+	 * Add one medal to the silver medals.
+	 */
+
 	public void addPrata() {
 		this.prata += 1;
 	}
+
+	/**
+	 * Add one medal to the bronze medals.
+	 */
 
 	public void addBronze() {
 		this.bronze += 1;
 	}
 
+	/**
+	 * @return returns a String in this format: "Gold: gold medals
+	 *         <p>
+	 *         Silver: silver medals
+	 *         <p>
+	 *         Bronze: bronze medals"
+	 */
 	@Override
 	public String toString() {
-		return String.format("Ouro: %d\nPrata: %d\nBronze: %d\n", this.ouro, this.prata, this.bronze);
+		return String.format("Gold: %d\nSilver: %d\nBronze: %d\n", this.ouro, this.prata, this.bronze);
 	}
 
 }
--- src/jogosolimpicos/Pais.java
+++ src/jogosolimpicos/Pais.java
@@ -1,40 +1,94 @@
 package jogosolimpicos;
 
+/**
+ * Main class of the country.
+ * 
+ */
 public class Pais {
 
 	private String codigoPais;
 	private String nomePais;
 	private Medalhas medalha;
 
+	/**
+	 * Main constructor of the class country.
+	 * 
+	 * @param codigoPais
+	 *            a string of the country's code with the IOC standart. Ex.:
+	 *            "POR"
+	 * @param nomePais
+	 *            name of the country. Ex.: "Portugal"
+	 */
+
 	public Pais(String codigoPais, String nomePais) {
 		setCodigoPais(codigoPais);
 		setNomePais(nomePais);
-
+		setMedalha();
 	}
 
+	/**
+	 * Get the country's code.
+	 * 
+	 * @return the code of country
+	 */
 	public String getCodigoPais() {
 		return codigoPais;
 	}
+
+	/**
+	 * Set the country's code.
+	 * 
+	 * @param codigoPais
+	 *            country's code with the IOC standart. Ex.: "POR"
+	 */
 
 	public void setCodigoPais(String codigoPais) {
 		this.codigoPais = codigoPais;
 	}
 
+	/**
+	 * Get the country's name.
+	 * 
+	 * @return the name of the country.
+	 */
+
 	public String getNomePais() {
 		return nomePais;
 	}
+
+	/**
+	 * Set the country's name.
+	 * 
+	 * @param nomePais
+	 *            the name of the country.
+	 */
 
 	public void setNomePais(String nomePais) {
 		this.nomePais = nomePais;
 	}
 
+	/**
+	 * Initializes the medals to zero.
+	 */
+
 	public void setMedalha() {
 		this.medalha = new Medalhas();
 	};
 
+	/**
+	 * Get the country's medals.
+	 * 
+	 * @return the medals of the country.
+	 */
+
 	public Medalhas getMedalha() {
 		return this.medalha;
 	}
+
+	/**
+	 * @return returns a String in this format: "countrycode - countryname"
+	 * 
+	 */
 
 	@Override
 	public String toString() {
