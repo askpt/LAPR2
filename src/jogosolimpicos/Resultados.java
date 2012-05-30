b27d7bf0ef1a42d615394693286849d107503c9e
JoniDragons2 <joni__joao@hotmail.com>
2012-05-30 11:39:15 +0000

    Modificação da classe resultados
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@17 d26f016a-5ee3-6924-6cc9-f88242385996

--- src/jogosolimpicos/Resultados.java
+++ src/jogosolimpicos/Resultados.java
@@ -1,16 +1,29 @@
 package jogosolimpicos;
 
-public class Resultados {
+public class Resultados implements Comparable {
 
-	private float resultados;
-	private Medalhas medalha;
+	private Atleta atleta;
+	private float resultado;
 
 	public Resultados(Atleta atleta, float resultado) {
 
 	}
 
-	public Resultados(Equipa equipa, float resultado) {
-
+	public float getResultado() {
+		return resultado;
 	}
 
+	public void setResultados(float resultado) {
+		this.resultado = resultado;
+	}
+
+	@Override
+	public int compareTo(Object outro) {
+		return (int) (this.getResultado() - ((Resultados) outro).getResultado());
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%.2f", resultado);
+	}
 }
