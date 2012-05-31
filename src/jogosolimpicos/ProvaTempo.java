473a3f921d341038e18de6c33053df75cf6c1565
vitorhugoanjos <vitorhugoanjosowns@gmail.com>
2012-05-31 11:41:31 +0000

    Update Provas
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@32 d26f016a-5ee3-6924-6cc9-f88242385996

---
+++ src/jogosolimpicos/ProvaTempo.java
@@ -0,0 +1,35 @@
+
+package jogosolimpicos;
+
+public class ProvaTempo extends Prova {
+
+	private String	tempo;
+	private float	tempoSec;
+
+	public ProvaTempo( Disciplina disc , JogosOlimpicos jogos , String tempo ) {
+
+		super( disc , jogos );
+		setTempo( tempo );
+		calculaSec();
+	}
+
+	private void calculaSec() {
+
+		if ( tempo.matches( "^[0-9]{2}:[0-9]{2}:[0-9]{2}$" ) ) {
+			String temp[] = tempo.split( ":" );
+			tempoSec = Float.parseFloat( temp[ 0 ] ) * 3600 + Float.parseFloat( temp[ 1 ] ) * 60 + Float.parseFloat( temp[ 2 ] );
+		} else if ( tempo.matches( "^[0-9]{1,2}:[0-9]{2}(.[0-9]{1,})?$" ) ) {
+			String temp[] = tempo.split( ":" );
+			tempoSec = Float.parseFloat( temp[ 0 ] ) * 60 + Float.parseFloat( temp[ 1 ] );
+
+		} else {
+			tempoSec = Float.parseFloat( tempo );
+		}
+	}
+
+	private void setTempo( String tempo ) {
+
+		this.tempo = tempo;
+	}
+
+}
