aa7ddba9cbf4f045491ff5626404824bff604c13
vitorhugoanjos <vitorhugoanjosowns@gmail.com>
2012-06-02 13:42:19 +0000

    tirei o atributo codPais do atleta porque dá para aceder apartir do país do atleta já
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@50 d26f016a-5ee3-6924-6cc9-f88242385996

--- src/jogosolimpicos/Atleta.java
+++ src/jogosolimpicos/Atleta.java
@@ -1,42 +1,36 @@
-
 package jogosolimpicos;
 
 import listaligada.*;
 
 public class Atleta {
 
-	private String					nome;
-	private String					codPais;
-	private int					numid;
-	private static int				num;
-	private Pais					pais;
-	private Modalidade				mod;
-	private ListaLigada < ProvaInd >	provas	= new ListaLigada < ProvaInd >();
-	private boolean				equipa	= false;
-	private Medalhas				medalha;
+	private String nome;
+	private int numid;
+	private static int num;
+	private Pais pais;
+	private Modalidade mod;
+	private ListaLigada<ProvaInd> provas = new ListaLigada<ProvaInd>();
+	private boolean equipa = false;
+	private Medalhas medalha;
 
-	public Atleta( String nome , Modalidade mod , Pais pais , ProvaInd prova , boolean equipa ) {
+	public Atleta(String nome, Modalidade mod, Pais pais, ProvaInd prova, boolean equipa) {
 		//
 		setID();
-		setNome( nome );
-		setModalidade( mod );
-		setPais( pais );
-		addProva( prova );
-		setEquipa( equipa );
+		setNome(nome);
+		setModalidade(mod);
+		setPais(pais);
+		addProva(prova);
+		setEquipa(equipa);
 		medalha = new Medalhas();
 	}
 
 	// GETTERS
-	public String getCodPais() {
 
-		return codPais;
-	}
+	public void setProvas(ListaLigada<ProvaInd> provas) {
 
-	public void setProvas( ListaLigada < ProvaInd > provas ) {
-
-		for ( int i = 0 ; i < this.provas.size() ; i++ ) {
-			if ( ! this.provas.contains( provas.get( i ) ) ) {
-				this.provas.add( provas.get( i ) );
+		for (int i = 0; i < this.provas.size(); i++) {
+			if (!this.provas.contains(provas.get(i))) {
+				this.provas.add(provas.get(i));
 			}
 		}
 	}
@@ -56,26 +50,22 @@ public class Atleta {
 		return pais;
 	}
 
-	public ListaLigada < ProvaInd > getProvas() {
+	public ListaLigada<ProvaInd> getProvas() {
 
 		return provas;
 	}
 
 	public String toString() {
 
-		return String.format( "%d: %s - %s" , numid , nome , pais );
+		return String.format("%d: %s - %s", numid, nome, pais);
 	}
 
 	// SETTERS
-	public void setCodPais( String codPais ) {
 
-		this.codPais = codPais;
-	}
+	public void addProva(ProvaInd prova) {
 
-	public void addProva( ProvaInd prova ) {
-
-		if ( ! provas.contains( prova ) ) {
-			provas.add( prova );
+		if (!provas.contains(prova)) {
+			provas.add(prova);
 		}
 	}
 
@@ -89,28 +79,28 @@ public class Atleta {
 		this.numid = ++num;
 	}
 
-	public void setNome( String nome ) {
+	public void setNome(String nome) {
 
 		this.nome = nome;
 	}
 
-	public void setPais( Pais pais ) {
+	public void setPais(Pais pais) {
 
 		this.pais = pais;
 	}
 
-	public void setModalidade( Modalidade mod ) {
+	public void setModalidade(Modalidade mod) {
 
 		this.mod = mod;
 	}
 
-	public void setEquipa( boolean equipa ) {
+	public void setEquipa(boolean equipa) {
 
 		this.equipa = equipa;
 
 	}
 
-	public void setMedalha( Medalhas medalha ) {
+	public void setMedalha(Medalhas medalha) {
 
 		this.medalha = medalha;
 	}
