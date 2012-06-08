b6785960ab7a87eb5c02a50db2d8b77e2ed1aaa1
JoniDragons2 <joni__joao@hotmail.com>
2012-06-08 12:09:42 +0000

    Update HTML
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@87 d26f016a-5ee3-6924-6cc9-f88242385996

---
+++ src/dados/HTML.java
@@ -0,0 +1,88 @@
+package dados;
+
+import gui.*;
+
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+
+import jogosolimpicos.*;
+import listaligada.*;
+
+public class HTML {
+
+	public void exportPais(Component janela, int anoInicio, int anoFim) throws FileNotFoundException {
+
+		if (!Main.getPaises().isEmpty()) {
+
+			JFileChooser fc = new JFileChooser();
+			fc.setFileFilter(new HTMLFilter());
+			int returnVal = fc.showSaveDialog(janela);
+			if (returnVal != JFileChooser.APPROVE_OPTION)
+				return;
+			File ficheiro = fc.getSelectedFile();
+			Formatter fout = new Formatter(ficheiro + ".html");
+			corpoInicio(fout);
+			ListaLigada auxAtletas = Main.getAtleta();
+			ListaLigada<Prova> provaTemp = Main.getProvas();
+			for (int i = anoInicio; i < anoFim; i += 4) {
+				for (int j = 0; j < provaTemp.size(); j++) {
+					if (provaTemp.get(j).getJogosOlimpicos().getAno() != i) {
+						provaTemp.remove(j);
+						j--;
+					}
+				}
+				for (int j = 0; j < provaTemp.size(); j++) {
+
+				}
+			}
+		} else {
+			JOptionPane.showMessageDialog(janela, "Não existem países para exportar", "Export", JOptionPane.INFORMATION_MESSAGE);
+			return;
+		}
+	}
+
+	public void corpoInicio(Formatter fout) {
+
+		fout.format("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\n");
+		fout.format("\"http://www.w3.org/TR/html4/loose.dtd\">");
+		fout.format("<head>");
+		fout.format("<title> Jogos Ol&iacutempicos");
+		fout.format("</title>");
+		fout.format("</head>");
+		fout.format("<body>");
+
+	}
+
+	private class HTMLFilter extends FileFilter {
+
+		@Override
+		public boolean accept(File f) {
+
+			if (f.isDirectory()) {
+				return true;
+			}
+			int ponto = f.getName().lastIndexOf(".");
+			String extensao = f.getName().substring(ponto + 1);
+
+			if (extensao != null) {
+				if (extensao.equalsIgnoreCase("html"))
+					return true;
+			} else
+				return false;
+
+			return false;
+
+		}
+
+		@Override
+		public String getDescription() {
+
+			return String.format("HTML File.");
+		}
+	}
+
+}
