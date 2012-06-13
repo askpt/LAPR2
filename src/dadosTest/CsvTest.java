b4e9badf963d8b1a5d9e758b364aa92ba9604186
askpt <andres.silva010@gmail.com>
2012-06-13 15:42:49 +0000

    CSVTest: adicionados testes unitários corrigidos. csv_tests: adicionados csv para testes unitários
    git-svn-id: http://lapr2-projeto.googlecode.com/svn@161 d26f016a-5ee3-6924-6cc9-f88242385996

--- src/dadosTest/CsvTest.java
+++ src/dadosTest/CsvTest.java
@@ -1,16 +1,18 @@
 package dadosTest;
 
 import static org.junit.Assert.*;
-import listaligada.*;
-import jogosolimpicos.*;
 
-import org.junit.Test;
-import dados.*;
+import java.io.*;
+
 import gui.*;
+import jogosolimpicos.*;
+import listaligada.*;
 
-//TODO falta acabar
+import org.junit.*;
+
+import dados.*;
+
 public class CsvTest {
-
 	@Test
 	public void testImportPais() {
 		ListaLigada<Pais> testeList = new ListaLigada<Pais>();
@@ -22,7 +24,7 @@ public class CsvTest {
 		esperadoList.add(new Pais("ALG", "Algeria"));
 
 		Csv csv = new Csv();
-		csv.importPais(null, new Teste("Test"), testeList);
+		csv.importPais(new File("csv_tests\\IOC_test_JUnit_importPais.csv"), new Teste("Test"), testeList);
 
 		for (int i = 0; i < testeList.size(); i++) {
 			if (!esperadoList.get(i).getCodigoPais(0).equals(testeList.get(i).getCodigoPais(0)) || !esperadoList.get(i).getNomePais().equals(testeList.get(i).getNomePais()))
@@ -32,32 +34,14 @@ public class CsvTest {
 		assertTrue(teste);
 	}
 
+	// TODO acabar
 	@Test
-	public void testExportPais() {
-		ListaLigada<Pais> esperadoList = new ListaLigada<Pais>();
-		boolean teste = true;
-
-		esperadoList.add(new Pais("AFG", "Afghanistan"));
-		esperadoList.add(new Pais("ALB", "Albania"));
-		esperadoList.add(new Pais("ALG", "Algeria"));
-
-		Csv csv = new Csv();
-		csv.exportPais(new Teste("Test"), esperadoList);
-
-		ListaLigada<Pais> testList = new ListaLigada<Pais>();
-		csv.importPais(null, new Teste("Test"), testList);
-		for (int i = 0; i < testList.size(); i++) {
-			if (!esperadoList.get(i).getCodigoPais(0).equals(testList.get(i).getCodigoPais(0)) || !esperadoList.get(i).getNomePais().equals(testList.get(i).getNomePais()))
-				teste = false;
-		}
-
-		assertTrue(teste);
-
+	public void testImportLingua() {
+		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testImportDisc() {
-
 		ListaLigada<Modalidade> esperadoModalList = new ListaLigada<Modalidade>();
 		ListaLigada<Disciplina> esperadoDiscList = new ListaLigada<Disciplina>();
 		ListaLigada<Modalidade> testeModalList = new ListaLigada<Modalidade>();
@@ -73,7 +57,7 @@ public class CsvTest {
 		esperadoDiscList.add(new Disciplina("Basketball", esperadoModalList.get(1), true, 0, false, 3));
 		esperadoDiscList.add(new Disciplina("Basketball", esperadoModalList.get(1), true, 1, false, 3));
 		Csv csv = new Csv();
-		csv.importDisc(null, new Teste("Test"), testeDiscList, testeModalList);
+		csv.importDisc(new File("csv_tests\\IOC_test_JUnit_importDisc.csv"), new Teste("Test"), testeDiscList, testeModalList);
 		for (int i = 0; i < esperadoModalList.size() && i < testeModalList.size() && teste; i++) {
 			if (!esperadoModalList.get(i).getNome().equalsIgnoreCase(testeModalList.get(i).getNome()))
 				teste = false;
@@ -87,39 +71,52 @@ public class CsvTest {
 		assertTrue(true);
 	}
 
+	// TODO acabar
 	@Test
-	public void testExportDisc() {
+	public void testImportResultados() {
+		fail("Not yet implemented");
+	}
 
-		ListaLigada<Modalidade> esperadoModalList = new ListaLigada<Modalidade>();
-		ListaLigada<Disciplina> esperadoDiscList = new ListaLigada<Disciplina>();
+	@Test
+	public void testImportProvas() {
+		Csv csv = new Csv();
+		ListaLigada<JogosOlimpicos> jogosTest = new ListaLigada<JogosOlimpicos>();
+		ListaLigada<Prova> provaTest = new ListaLigada<Prova>();
+		ListaLigada<Disciplina> discTest = new ListaLigada<Disciplina>();
+		ListaLigada<Modalidade> modalTest = new ListaLigada<Modalidade>();
+
+		ListaLigada<JogosOlimpicos> jogosEsp = new ListaLigada<JogosOlimpicos>();
+		ListaLigada<Prova> provaEsp = new ListaLigada<Prova>();
+		ListaLigada<Disciplina> discEsp = new ListaLigada<Disciplina>();
+		ListaLigada<Modalidade> modalEsp = new ListaLigada<Modalidade>();
+
+		modalEsp.add(new Modalidade("Athletics"));
+		modalEsp.add(new Modalidade("Basketball"));
+		discEsp.add(new Disciplina("High Jump", modalEsp.get(0), false, 0, true, 0));
+		discEsp.add(new Disciplina("High Jump", modalEsp.get(0), false, 1, true, 0));
+		discEsp.add(new Disciplina("Long Jump", modalEsp.get(0), false, 0, true, 0));
+		discEsp.add(new Disciplina("Long Jump", modalEsp.get(0), false, 1, true, 0));
+		discEsp.add(new Disciplina("Basketball", modalEsp.get(1), true, 0, false, 3));
+		discEsp.add(new Disciplina("Basketball", modalEsp.get(1), true, 1, false, 3));
+		jogosEsp.add(new JogosOlimpicos(1996));
+		provaEsp.add(new ProvaInd(discEsp.get(0), jogosEsp.get(0)));
+		provaEsp.add(new ProvaInd(discEsp.get(2), jogosEsp.get(0)));
+		provaEsp.add(new ProvaInd(discEsp.get(3), jogosEsp.get(0)));
+		csv.importDisc(new File("csv_tests\\IOC_test_JUnit_importDisc.csv"), new Teste("Test"), discTest, modalTest);
+		csv.importProvas(new File("csv_tests\\IOC_Sports_OG_1996.csv"), new Teste("Test"), jogosTest, provaTest, discTest, modalTest);
 
 		boolean teste = true;
-		esperadoModalList.add(new Modalidade("Athletics"));
-		esperadoModalList.add(new Modalidade("Basketball"));
-		esperadoDiscList.add(new Disciplina("High Jump", esperadoModalList.get(0), false, 0, true, 0));
-		esperadoDiscList.add(new Disciplina("High Jump", esperadoModalList.get(0), false, 1, true, 0));
-		esperadoDiscList.add(new Disciplina("Long Jump", esperadoModalList.get(0), false, 0, true, 0));
-		esperadoDiscList.add(new Disciplina("Long Jump", esperadoModalList.get(0), false, 1, true, 0));
-		esperadoDiscList.add(new Disciplina("Basketball", esperadoModalList.get(1), true, 0, false, 3));
-		esperadoDiscList.add(new Disciplina("Basketball", esperadoModalList.get(1), true, 1, false, 3));
-		Csv csv = new Csv();
-		csv.exportDisciplina(new Teste("Test"), esperadoModalList);
-
-		ListaLigada<Modalidade> testeModalList = new ListaLigada<Modalidade>();
-		ListaLigada<Disciplina> testeDiscList = new ListaLigada<Disciplina>();
-		csv.importDisc(null, new Teste("Test"), testeDiscList, testeModalList);
-
-		for (int i = 0; i < esperadoModalList.size() && i < testeModalList.size() && teste; i++) {
-			if (!esperadoModalList.get(i).getNome().equalsIgnoreCase(testeModalList.get(i).getNome()))
+		for (int i = 0; i < modalEsp.size() && i < modalTest.size() && teste; i++) {
+			if (!modalEsp.get(i).getNome().equalsIgnoreCase(modalTest.get(i).getNome()))
 				teste = false;
 		}
 
-		for (int i = 0; i < esperadoDiscList.size() && i < testeDiscList.size() && teste; i++) {
-			if (!esperadoDiscList.get(i).getNome().equalsIgnoreCase(testeDiscList.get(i).getNome()) || !esperadoDiscList.get(i).getModalidade().getNome().equalsIgnoreCase(testeDiscList.get(i).getModalidade().getNome()) || !esperadoDiscList.get(i).getOrdenacao() == testeDiscList.get(i).getOrdenacao() || !esperadoDiscList.get(i).getTipoMod() == testeDiscList.get(i).getTipoMod())
+		for (int i = 0; i < discEsp.size() && i < discTest.size() && teste; i++) {
+			if (!discEsp.get(i).getNome().equalsIgnoreCase(discTest.get(i).getNome()) || !discEsp.get(i).getModalidade().getNome().equalsIgnoreCase(discTest.get(i).getModalidade().getNome()) || !discEsp.get(i).getOrdenacao() == discTest.get(i).getOrdenacao() || !discEsp.get(i).getTipoMod() == discTest.get(i).getTipoMod())
 				teste = false;
 		}
 
-		assertTrue(true);
+		assertTrue(teste);
 	}
 
 }
